680b91ca7db968bfda56f3f1c3710a381d6ffa32
Gabriel da Silva Loures <gabriel.loures1908@gmail.com>
2025-07-02 19:30:02 -0300

    Atualizações

---
+++ exercicios/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+function App() {
+
+    const saudacao = <Text>Olá Mundo</Text>
+
+    return saudacao
+
+}
+
+export default App;
